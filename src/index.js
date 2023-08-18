af86bb64e3a9bcd7ff5c07a90a102b27204c0795
john-nash-rs <rohitsingh20121992@gmail.com>
2023-08-18 17:13:39 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// src/index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ThemeProvider } from '@mui/material/styles';
+import App from './App';
+import theme from './styles/theme';
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById('root')
+);
---
+++ src/styles/theme.js
@@ -0,0 +1,14 @@
+// src/themes/theme.js
+
+import { createTheme } from '@mui/material/styles';
+import { deepPurple } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: deepPurple[500],
+    },
+  },
+});
+
+export default theme;
